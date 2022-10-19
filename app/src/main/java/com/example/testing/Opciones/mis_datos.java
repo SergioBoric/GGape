6275347eb07152e0ef28debc757a2c8f8e3351e0
Juan <juanfelipecuatro9@gmail.com>
2022-10-18 22:45:46 -0300

    qqq

---
+++ app/src/main/java/com/example/testing/Opciones/mis_datos.java
@@ -0,0 +1,14 @@
+package com.example.testing.Opciones;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mis_datos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mis_datos);
+    }
+}
