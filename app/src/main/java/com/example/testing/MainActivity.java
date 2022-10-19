b181b263c6a1dd73246c6b23b100586e5924a498
SemeaUwU <113060652+SemeaUwU@users.noreply.github.com>
2022-10-19 10:53:54 -0300

    aa

--- app/src/main/java/com/example/testing/MainActivity.java
+++ app/src/main/java/com/example/testing/MainActivity.java
@@ -11,8 +11,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.basgeekball.awesomevalidation.AwesomeValidation;
-import com.basgeekball.awesomevalidation.ValidationStyle;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -25,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
     Button btnl,btnr;
     EditText Elogin,Plogin;
 
-    AwesomeValidation awesomeValidation;
     FirebaseAuth firebaseAuth;
 
     @Override
@@ -35,145 +32,20 @@ public class MainActivity extends AppCompatActivity {
         btnl = findViewById(R.id.buttonLogin);
         btnr = findViewById(R.id.buttonregister);
 
-        Elogin = findViewById(R.id.EmailLogin);
-        Plogin = findViewById(R.id.PasswordLogin);
-
-        firebaseAuth = FirebaseAuth.getInstance();
-
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-
-        FirebaseUser user = mAuth.getCurrentUser();
-
-        if (user != null){
-            irahome();
-        }
-
-        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
-        awesomeValidation.addValidation(this,R.id.Email, Patterns.EMAIL_ADDRESS,R.string.invalid_mail);
-        awesomeValidation.addValidation(this,R.id.Password,".{6,}",R.string.invalid_password);
-
         btnl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (awesomeValidation.validate()){
+                startActivity(new Intent(MainActivity.this,Inicio.class));
 
-                    String mail = Elogin.getText().toString();
-                    String pass = Plogin.getText().toString();
-
-                    firebaseAuth.signInWithEmailAndPassword(mail,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-
-                            if (task.isSuccessful()){
-                                irahome();
-
-                            }else {
-                                String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
-                                dameToastdeerror(errorCode);
-                            }
-
-                        }
-                    });
-
-                }
             }
         });
+
         btnr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, registrarse.class);
-                startActivity(intent);
+                startActivity(new Intent(MainActivity.this,Registro.class));
             }
         });
-
-    }
-
-
-    private void dameToastdeerror(String error) {
-
-        switch (error) {
-
-            case "ERROR_INVALID_CUSTOM_TOKEN":
-                Toast.makeText(MainActivity.this, "El formato del token personalizado es incorrecto. Por favor revise la documentación", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_CUSTOM_TOKEN_MISMATCH":
-                Toast.makeText(MainActivity.this, "El token personalizado corresponde a una audiencia diferente.", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_INVALID_CREDENTIAL":
-                Toast.makeText(MainActivity.this, "La credencial de autenticación proporcionada tiene un formato incorrecto o ha caducado.", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_INVALID_EMAIL":
-                Toast.makeText(MainActivity.this, "La dirección de correo electrónico está mal formateada.", Toast.LENGTH_LONG).show();
-                Elogin.setError("La dirección de correo electrónico está mal formateada.");
-                Elogin.requestFocus();
-                break;
-
-            case "ERROR_WRONG_PASSWORD":
-                Toast.makeText(MainActivity.this, "La contraseña no es válida o el usuario no tiene contraseña.", Toast.LENGTH_LONG).show();
-                Plogin.setError("la contraseña es incorrecta ");
-                Plogin.requestFocus();
-                Plogin.setText("");
-                break;
-
-            case "ERROR_USER_MISMATCH":
-                Toast.makeText(MainActivity.this, "Las credenciales proporcionadas no corresponden al usuario que inició sesión anteriormente..", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_REQUIRES_RECENT_LOGIN":
-                Toast.makeText(MainActivity.this, "Esta operación es sensible y requiere autenticación reciente. Inicie sesión nuevamente antes de volver a intentar esta solicitud.", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL":
-                Toast.makeText(MainActivity.this, "Ya existe una cuenta con la misma dirección de correo electrónico pero diferentes credenciales de inicio de sesión. Inicie sesión con un proveedor asociado a esta dirección de correo electrónico.", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_EMAIL_ALREADY_IN_USE":
-                Toast.makeText(MainActivity.this, "La dirección de correo electrónico ya está siendo utilizada por otra cuenta..   ", Toast.LENGTH_LONG).show();
-                Elogin.setError("La dirección de correo electrónico ya está siendo utilizada por otra cuenta.");
-                Elogin.requestFocus();
-                break;
-
-            case "ERROR_CREDENTIAL_ALREADY_IN_USE":
-                Toast.makeText(MainActivity.this, "Esta credencial ya está asociada con una cuenta de usuario diferente.", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_USER_DISABLED":
-                Toast.makeText(MainActivity.this, "La cuenta de usuario ha sido inhabilitada por un administrador..", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_USER_TOKEN_EXPIRED":
-                Toast.makeText(MainActivity.this, "La credencial del usuario ya no es válida. El usuario debe iniciar sesión nuevamente.", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_USER_NOT_FOUND":
-                Toast.makeText(MainActivity.this, "No hay ningún registro de usuario que corresponda a este identificador. Es posible que se haya eliminado al usuario.", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_INVALID_USER_TOKEN":
-                Toast.makeText(MainActivity.this, "La credencial del usuario ya no es válida. El usuario debe iniciar sesión nuevamente.", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_OPERATION_NOT_ALLOWED":
-                Toast.makeText(MainActivity.this, "Esta operación no está permitida. Debes habilitar este servicio en la consola.", Toast.LENGTH_LONG).show();
-                break;
-
-            case "ERROR_WEAK_PASSWORD":
-                Toast.makeText(MainActivity.this, "La contraseña proporcionada no es válida..", Toast.LENGTH_LONG).show();
-                Plogin.setError("La contraseña no es válida, debe tener al menos 6 caracteres");
-                Plogin.requestFocus();
-                break;
-        }
-    }
-
-    private void irahome(){
-
-        Intent i = new Intent(this, MainActivity2.class);
-        i.putExtra("mail",Elogin.getText().toString());
-        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP/Intent.FLAG_ACTIVITY_CLEAR_TASK/Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(i);
     }
 
 }
