6d27761c0f59c8d1d4cc5251dbf3e3156f0b258a
Juan <juanfelipecuatro9@gmail.com>
2022-10-23 03:20:22 -0300

    PantallaDeCarga 1seg

--- app/src/main/java/com/example/testing/PantallaDeCarga.java
+++ app/src/main/java/com/example/testing/PantallaDeCarga.java
@@ -2,7 +2,9 @@ package com.example.testing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class PantallaDeCarga extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class PantallaDeCarga extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_de_carga);
+
+        final int Duracion = 1000; //Tiempo
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Esto se ejecutara los segundos
+                Intent intent = new Intent( PantallaDeCarga.this, MainActivity.class);
+                startActivity(intent);
+                //Nos dirige al mainactivity
+
+            }
+        },Duracion);
     }
+
+
+
 }
