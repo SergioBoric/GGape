e5a0b754e0d1bc647921c5677fc088e320a264fa
Juan <juanfelipecuatro9@gmail.com>
2022-10-26 00:38:14 -0300

    Se implementa la especialidad y el rut

--- app/src/main/java/app/dominio/testing/DisplayUsuarios.java
+++ app/src/main/java/app/dominio/testing/DisplayUsuarios.java
@@ -1,22 +1,38 @@
 package app.dominio.testing;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 
 import app.dominio.testing.Adapter.UserAdapter;
 import app.dominio.testing.Model.User;
 
 import com.example.testing.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DisplayUsuarios extends AppCompatActivity {
 
-    RecyclerView recyclerUsuarios;
+
+    Button emailEnviar;
+
+    RecyclerView recyclerView;
+    DatabaseReference database;
+    UserAdapter myAdapter;
+    ArrayList<User> list;
 
 
     @Override
@@ -24,5 +40,38 @@ public class DisplayUsuarios extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_maestros);
 
+
+
+        recyclerView = findViewById(R.id.recycler);
+        database = FirebaseDatabase.getInstance().getReference("USUARIOS_DE_APP");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        list = new ArrayList<>();
+        myAdapter = new UserAdapter(this, list);
+        recyclerView.setAdapter(myAdapter);
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    User user = dataSnapshot.getValue(User.class);
+                    list.add(user);
+                }
+                myAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
     }
+
+
+
+
 }
